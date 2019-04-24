e8f3ad283bf301c06dfd18f08a8030a7f858b949
DaniiikWAA <d.hajurkova@gmail.com>
2019-04-24 18:41:05 +0200

    moj happy commit cislo dva :)
    pridali sme testy a skusali sme css lokatory
    
    - pridanie testu warning message
    - pridanie testu title stranky
    - pridanie overovania sklonovania v pocte ryanov

--- src/test/java/tests/GosslingatorTest.java
+++ src/test/java/tests/GosslingatorTest.java
@@ -6,12 +6,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+
 public class GosslingatorTest {
 
     //TENTO MA PASS
     @Test
     public void itShouldAddOneRyan() {
-        System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "chromedriver74.exe");
         //nazov testu, mal by ho vystihovat
         //0. spustit prehliadac
         WebDriver driver = new ChromeDriver();
@@ -24,17 +25,99 @@ public class GosslingatorTest {
         driver.findElement(By.id("addRyan")).click();
         driver.findElement(By.id("removeRyan")).click();
         //3. overit pocitanie Ryanov
-        Assert.assertEquals("3", driver.findElement(By.id("ryanCounter")).getText());
+        Assert.assertEquals("3", driver.findElement(By.cssSelector("div.ryan-counter h2")).getText());
+        System.out.println("Number of Ryans: " + driver.findElement(By.cssSelector("div.ryan-counter h2")).getText());
+        Assert.assertEquals("ryans", driver.findElement(By.cssSelector("div.ryan-counter h3")).getText());
         //4. zatvorit prehliadac
         driver.close();
         //5. ukoncit session
         driver.quit();
     }
 
+    //NOVY TEST
+    @Test
+    public void itShouldDisplayTitle() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver74.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://localhost:81/gosslingator.php");
+        System.out.println("Nazov na stranke je: " + driver.findElement(By.cssSelector("h1.ryan-title")).getText());
+        Assert.assertEquals("GOSLINGATE ME", driver.findElement(By.cssSelector("h1.ryan-title")).getText());
+        driver.close();
+        driver.quit();
+    }
+
+    //ULOHA
+    @Test
+    public void itShouldChecktheMessage() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver74.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://localhost:81/gosslingator.php");
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        driver.findElement(By.id("addRyan")).click();
+        Assert.assertEquals("50", driver.findElement(By.cssSelector("div.ryan-counter h2")).getText());
+        System.out.println("Number of Ryans: " + driver.findElement(By.cssSelector("div.ryan-counter h2")).getText());
+        Assert.assertEquals("ryans", driver.findElement(By.cssSelector("div.ryan-counter h3")).getText());
+        System.out.println("Hlaska: " + driver.findElement(By.cssSelector("h1.tooManyRyans")).getText());
+        Assert.assertEquals("NUMBER OF\n" +
+                "RYANS\n" +
+                "IS TOO DAMN\n" +
+                "HIGH", driver.findElement(By.cssSelector("h1.tooManyRyans")).getText());
+        driver.close();
+        driver.quit();
+    }
+
     //TENTO MA FAILNUT
     @Test
     public void itShouldAddTwoRyan() {
-        System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "chromedriver74.exe");
         //nazov testu, mal by ho vystihovat
         //0. spustit prehliadac
         WebDriver driver = new ChromeDriver();
@@ -46,7 +129,21 @@ public class GosslingatorTest {
         driver.findElement(By.id("addRyan")).click();
         driver.findElement(By.id("removeRyan")).click();
         //3. overit pocitanie Ryanov
-        Assert.assertEquals("1", driver.findElement(By.id("ryanCounter")).getText());
+        Assert.assertEquals("2", driver.findElement(By.id("ryanCounter")).getText());
+        //4. zatvorit prehliadac
+        driver.close();
+        //5. ukoncit session
+        driver.quit();
+    }
+
+    //HOMEWORK
+    @Test
+    public void itShouldOpenthePage() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver74.exe");
+        //0. spustit prehliadac
+        WebDriver driver = new ChromeDriver();
+        //1. otvorit stranku
+        driver.get("http://localhost:81/vybersi.php");
         //4. zatvorit prehliadac
         driver.close();
         //5. ukoncit session
